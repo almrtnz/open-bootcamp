cc2692fa5eadcc7cd587bb828956dd9313039411
almrtnz <hi@alwrks.com>
2022-08-20 19:44:31 +0200

    Ejercicios tema 3

--- 00-intro-programacion/ejercicios-tema-3/src/App.java
+++ 00-intro-programacion/ejercicios-tema-3/src/App.java
@@ -1,5 +1,36 @@
 public class App {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+        // Decir hola
+        System.out.println("Hello, OpenBootcamp!"); 
+        
+        int resultado; // la variable que guarda los datos
+        resultado = suma(23, 18, 56); // suma de tres números
+        
+        System.out.println("El resultado de la suma es " + resultado); 
+        // mostrar el resultado (23 + 18 + 56 = 97)
+
+        // Creación de objeto con propiedades (Segunda parte)
+        coche miCoche = new coche();
+        miCoche.NumPuertas();
+        System.out.println("Mi coche tiene " + miCoche.puertas + " puertas");
+    }
+
+    // Función con tres parámetros que sean números que se suman entre sí
+    public static int suma(int a, int b, int c){
+        
+        return a + b + c;
+    }
+
+    // Clase coche (Segunda parte)
+    static class coche {
+        public int puertas = 5;
+
+        // Función para incrementar el número de puertas
+        public void NumPuertas(){
+            this.puertas++;
+        }
+
     }
 }
